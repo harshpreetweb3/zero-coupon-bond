3fa4f225cb5d009c43039f3ae34e7f1e34ab21e7
harshpreetweb3 <harshpreetweb3@gmail.com>
2024-07-04 21:35:00 +0530

    bonds being minted and purchased

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,22 @@
 use scrypto::prelude::*;
 
+#[derive(ScryptoSbor, Debug)]
+pub struct BondDetails {
+    pub contract_type: String,
+    pub contract_role: String,
+    pub contract_identifier: String,
+    pub nominal_interest_rate: Decimal,
+    pub currency: String,
+    pub initial_exchange_date: u64,
+    pub maturity_date: u64,
+    pub notional_principal: Decimal,
+    pub discount: u64,
+    pub bond_position: String,
+    pub price: Decimal,
+    pub amount: Decimal,
+    pub maturity_days_left: i64,
+}
+
 #[blueprint]
 mod zerocouponbond {
 
@@ -8,12 +25,15 @@ mod zerocouponbond {
         contract_role: String,
         contract_identifier: String,
         nominal_interest_rate: Decimal,
-        // day_count_convention: String,
         currency: String,
         initial_exchange_date: u64,
         maturity_date: u64,
         notional_principal: Decimal,
         discount: u64,
+        bond_position: String,
+        bonds: Vault,
+        collected_xrd: Vault,
+        price: Decimal,
     }
 
     impl ZeroCouponBond {
@@ -27,60 +47,128 @@ mod zerocouponbond {
         // Token will represent a bond
         // Token would be of some face value eg 1000; we do have to give some face value to the bond
 
+        // PAM issuer CONTRACT1234 0.04 USD 1720100602 1727876602 1000 100 long 900 100
         pub fn instantiate_zerocouponbond(
             contract_type: String,          // PAM
             contract_role: String,          // issuer
             contract_identifier: String,    // unique id for a contract
             nominal_interest_rate: Decimal, // how much interest is being given by a bond
-            // day_count_convention: String,   // 30/360; this needs to be optimized for us  ; on hold
-            currency: String,               // for example : USD
+            currency: String,               // for example : XRD
             initial_exchange_date: u64,     // initial exchange date
             maturity_date: u64,             // date when bond matures
             notional_principal: Decimal,    // price defined by bond creator
             discount: u64,                  // discount on bond
+            bond_position: String,          // long or short position
+            price: Decimal,                 // price per bond
+            number_of_bonds: Decimal,       // number of bonds to mint
         ) -> Global<ZeroCouponBond> {
+            let bucket_of_bonds: Bucket = ResourceBuilder::new_fungible(OwnerRole::None)
+                .divisibility(DIVISIBILITY_NONE)
+                .metadata(metadata!(
+                    init {
+                        "name" => "ZeroCouponBond", locked;
+                        "symbol" => "ZCB", locked;
+                        "description" => "A Zero Coupon Bond", locked;
+                    }
+                ))
+                .mint_initial_supply(number_of_bonds)
+                .into();
+
             Self {
-                    contract_type,
-                    contract_role,
-                    contract_identifier,
-                    nominal_interest_rate,
-                    currency,
-                    initial_exchange_date,
-                    maturity_date,
-                    notional_principal,
-                    discount,
+                contract_type,
+                contract_role,
+                contract_identifier,
+                nominal_interest_rate,
+                currency,
+                initial_exchange_date,
+                maturity_date,
+                notional_principal,
+                discount,
+                bond_position,
+                bonds: Vault::with_bucket(bucket_of_bonds),
+                collected_xrd: Vault::new(XRD),
+                price,
             }
             .instantiate()
             .prepare_to_globalize(OwnerRole::None)
             .globalize()
         }
 
-        pub fn get_contract_details(
-            &self,
-        ) -> (
-            String,
-            String,
-            String,
-            Decimal,
-            // String,
-            String,
-            u64,
-            u64,
-            Decimal,
-            u64,
-        ) {
-            (
-                self.contract_type.clone(),
-                self.contract_role.clone(),
-                self.contract_identifier.clone(),
-                self.nominal_interest_rate,
-                // self.day_count_convention.clone(),
-                self.currency.clone(),
-                self.initial_exchange_date,
-                self.maturity_date,
-                self.notional_principal,
-                self.discount,
-            )
+        pub fn purchase_bond(&mut self, mut payment: Bucket) -> (Bucket, Bucket) {
+            let our_share = payment.take(self.price);
+            self.collected_xrd.put(our_share);
+            (self.bonds.take(1), payment)
+        }
+
+        pub fn sell_the_bond(&mut self, bond: Bucket) -> Bucket {
+            assert!(
+                bond.amount() == Decimal::one(),
+                "You can only sell one bond at a time."
+            );
+            assert!(
+                bond.resource_address() == self.bonds.resource_address(),
+                "Invalid bond resource."
+            );
+
+            self.bonds.put(bond);
+            let refund_amount = self.notional_principal + Decimal::from(self.discount);
+            self.collected_xrd.take(refund_amount)
+        }
+
+        // pub fn check_the_maturity_of_bonds(&self) -> i64 {
+        //     let current_timestamp = Runtime::current_epoch().number();
+        //     let days_left = (self.maturity_date as i64 - current_timestamp as i64) / (24 * 60 * 60);
+        //     days_left
+        // }
+
+        // pub fn get_bond_details(&self) -> BondDetails {
+        //     let current_timestamp = Runtime::current_epoch().number();
+        //     let days_left = (self.maturity_date as i64 - current_timestamp as i64) / (24 * 60 * 60);
+
+        //     BondDetails {
+        //         contract_type: self.contract_type.clone(),
+        //         contract_role: self.contract_role.clone(),
+        //         contract_identifier: self.contract_identifier.clone(),
+        //         nominal_interest_rate: self.nominal_interest_rate,
+        //         currency: self.currency.clone(),
+        //         initial_exchange_date: self.initial_exchange_date,
+        //         maturity_date: self.maturity_date,
+        //         notional_principal: self.notional_principal,
+        //         discount: self.discount,
+        //         bond_position: self.bond_position.clone(),
+        //         price: self.price,
+        //         amount: self.bonds.amount(),
+        //         maturity_days_left: days_left,
+        //     }
+        // }
+
+        pub fn check_the_maturity_of_bonds(&self) -> i64 {
+            let current_epoch = Runtime::current_epoch().number();
+            let seconds_in_day = 24 * 60 * 60;
+            let days_left = (self.maturity_date as i64 - current_epoch as i64) / seconds_in_day;
+            days_left
+        }
+
+        pub fn get_bond_details(&self) -> BondDetails {
+            let current_epoch = Runtime::current_epoch().number();
+            let seconds_in_day = 24 * 60 * 60;
+            let days_left = (self.maturity_date as i64 - current_epoch as i64) / seconds_in_day;
+        
+            BondDetails {
+                contract_type: self.contract_type.clone(),
+                contract_role: self.contract_role.clone(),
+                contract_identifier: self.contract_identifier.clone(),
+                nominal_interest_rate: self.nominal_interest_rate,
+                currency: self.currency.clone(),
+                initial_exchange_date: self.initial_exchange_date,
+                maturity_date: self.maturity_date,
+                notional_principal: self.notional_principal,
+                discount: self.discount,
+                bond_position: self.bond_position.clone(),
+                price: self.price,
+                amount: self.bonds.amount(),
+                maturity_days_left: days_left,
+            }
         }
 
         // function to check the price of the bond
@@ -94,11 +182,22 @@ mod zerocouponbond {
         // To whom it will be sold ?
 
         // track after maturity period
-
-
     }
 }
 
+// resim call-function package_sim1pk3cmat8st4ja2ms8mjqy2e9ptk8y6cx40v4qnfrkgnxcp2krkpr92 ZeroCouponBond instantiate_zerocouponbond PAM issuer CONTRACT1234 0.04 USD 1720100602 1727876602 1000 100 long 900 100
+// component_sim1cp4qmcqlmtsqns8ckwjttvffjk4j4smkhlkt0qv94caftlj5u2xve2
+// resim show component_sim1cp4qmcqlmtsqns8ckwjttvffjk4j4smkhlkt0qv94caftlj5u2xve2
+// resim show account_sim1c956qr3kxlgypxwst89j9yf24tjc7zxd4up38x37zr6q4jxdx9rhma
+
+// resim call-method component_sim1cp4qmcqlmtsqns8ckwjttvffjk4j4smkhlkt0qv94caftlj5u2xve2 get_bond_details 
+// resim call-method component_sim1cp4qmcqlmtsqns8ckwjttvffjk4j4smkhlkt0qv94caftlj5u2xve2 purchase_bond resource_sim1tknxxxxxxxxxradxrdxxxxxxxxx009923554798xxxxxxxxxakj8n3:900
+// resim call-method component_sim1cp4qmcqlmtsqns8ckwjttvffjk4j4smkhlkt0qv94caftlj5u2xve2 check_the_maturity_of_bonds
+
+ 
+
+
 
 // idea
-// instead of the IED and MD, we can take maturity_period such as 3M/90D, then after 3M/90D, amount will be repaid ... 
+// instead of the IED and MD, we can take maturity_period such as 3M/90D, then after 3M/90D, amount will be repaid ...
+
